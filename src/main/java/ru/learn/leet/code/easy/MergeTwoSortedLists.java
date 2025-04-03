b5816caa3452326295310e333565b1e184f3cd68
MSolodkov <MSolodkov@inno.tech>
2025-04-03 20:07:16 +0300

    add

--- src/main/java/ru/learn/leet/code/easy/MergeTwoSortedLists.java
+++ src/main/java/ru/learn/leet/code/easy/MergeTwoSortedLists.java
@@ -4,21 +4,26 @@ import ru.learn.leet.code.util.ListNode;
 
 public class MergeTwoSortedLists {
     public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode dummy = new ListNode();
-        ListNode curr = dummy;
-
-        while(list1 != null && list2 != null) {
-            if(list1.val > list2.val) {
-                curr.next = list2;
+        ListNode head = new ListNode();
+        ListNode res = head;
+        while (list1 != null && list2 != null) {
+            if (list1.val >= list2.val) {
+                res.next = list2;
                 list2 = list2.next;
-            } else{
-                curr.next = list1;
+            } else {
+                res.next = list1;
                 list1 = list1.next;
             }
-            curr = curr.next;
+            System.out.println(res.val);
+            res = res.next;
         }
+        if (list1 != null) {
+            res.next = list1;
+        }else {
+            res.next = list2;
+        }
+        return head.next;
 
-        return dummy.next;
     }
 
 
@@ -26,6 +31,7 @@ public class MergeTwoSortedLists {
     public static void main(String[] args) {
         System.out.println(mergeTwoLists(new ListNode(5),
                                          new ListNode(1, new ListNode(2, new ListNode(3)))));
+
         System.out.println(mergeTwoLists(new ListNode(1, new ListNode(2, new ListNode(3))),
                                          new ListNode(2, new ListNode(3, new ListNode(4)))));
     }
